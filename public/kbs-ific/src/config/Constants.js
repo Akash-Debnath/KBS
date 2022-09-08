c42eab1e52fab2f5efe6a88b401d7c8221fcf2d5
Akash Chandra Debnath <66691792+Akash-Debnath@users.noreply.github.com>
2022-09-08 14:44:09 +0600

    kbs back

---
+++ public/kbs-ific/src/config/Constants.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://kbs.gplex.com/v3.0/back/public/api/";
+// export const FILE_URL = "http://localhost/pms/pms1.1/public/media/attachment/";
+// export const CLIENT_IMG_URL = "http://localhost/pms/pms1.1/public/media/clients/";
+
+export const BASEPATH = "/";
